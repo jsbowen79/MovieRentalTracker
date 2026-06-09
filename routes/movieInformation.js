93d452175fc373340739c6fc57d75a89f607a3d1
budujoseph <budu.joseph95@gmail.com>
2026-06-09 21:38:19 +0000

    movie information validation and auth, added errors array to validationerrors function to display validation errors

--- routes/movieInformation.js
+++ routes/movieInformation.js
@@ -1,11 +1,34 @@
 const router = require('express').Router();
 const AsyncHandler = require('../errors/AsyncHandler.js');
 const movieInfoController = require('../controllers/movieInformation.js');
+const { movieInfoRules, validateMovieInfo } = require('../middleware/movieInfoValidate.js');
+const authenticateUser = require('../middleware/authorizeUser.js');
 
-router.get('/', AsyncHandler(movieInfoController.getAllMovies));
-router.get('/:id', AsyncHandler(movieInfoController.getMovieById));
-router.put('/:id', AsyncHandler(movieInfoController.updateMovieInfo));
-router.post('/', AsyncHandler(movieInfoController.addMovie));
-router.delete('/:id', AsyncHandler(movieInfoController.deleteMovie));
+router.get(
+    '/',
+    AsyncHandler(movieInfoController.getAllMovies));
+
+router.get(
+    '/:id',
+    AsyncHandler(movieInfoController.getMovieById));
+
+router.put(
+    '/:id',
+    authenticateUser('admin'),
+    movieInfoRules(),
+    validateMovieInfo,
+    AsyncHandler(movieInfoController.updateMovieInfo));
+
+router.post(
+    '/',
+    authenticateUser('admin'),
+    movieInfoRules(),
+    validateMovieInfo,
+    AsyncHandler(movieInfoController.addMovie));
+
+router.delete(
+    '/:id',
+    authenticateUser('admin'),
+    AsyncHandler(movieInfoController.deleteMovie));
 
 module.exports = router;
