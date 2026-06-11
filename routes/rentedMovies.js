3607295873da4326c4411e3846dbe4816ed9db2c
jsbowen79 <jsbowen79@outlook.com>
2026-06-11 12:35:43 -0600

    Delete uneccessary console logs

--- routes/rentedMovies.js
+++ routes/rentedMovies.js
@@ -3,24 +3,36 @@ const AsyncHandler = require('../errors/AsyncHandler.js');
 const rentController = require('../controllers/rentedMovies.js');
 const requireAuth = require('../middleware/requireAuth.js');
 const authorizeUser = require('../middleware/authorizeUser.js');
+const {validateNew, validateUpdate, validRentalRules, updateTransactionRules} =require('../controllers/rentedMovies_validator.js');
 
-router.post('/:userId', requireAuth, AsyncHandler(rentController.rentMovie));
+
+router.post('/:userId', 
+  requireAuth,
+  validRentalRules(), 
+  validateNew,
+  AsyncHandler(rentController.rentMovie));
+
 router.put(
   '/:transId',
   requireAuth,
+  updateTransactionRules(),
+  validateUpdate,
   AsyncHandler(rentController.updateTransaction)
 );
+
 router.get('/', requireAuth, AsyncHandler(rentController.listRentedMovies));
 router.get(
   '/:userId',
   authorizeUser('admin'),
   AsyncHandler(rentController.listRentedByUser)
 );
+
 router.get(
   '/out/:userId',
   authorizeUser('admin'),
   AsyncHandler(rentController.listRentedByUser)
 );
+
 router.delete(
   '/:transId',
   requireAuth,
