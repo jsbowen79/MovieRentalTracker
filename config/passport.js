e0daba0fce80fa38622b31b8fd74f2b868d3925e
jsbowen79 <jsbowen79@outlook.com>
2026-06-13 16:39:03 -0600

    fix movie information tag

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ passport.use(
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       callbackURL:
-      'https://movierentaltracker.onrender.com/auth/github/callback',
+        'https://movierentaltracker.onrender.com/auth/github/callback',
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
--- routes/movieInformation.js
+++ routes/movieInformation.js
@@ -1,34 +1,36 @@
 const router = require('express').Router();
 const AsyncHandler = require('../errors/AsyncHandler.js');
 const movieInfoController = require('../controllers/movieInformation.js');
-const { movieInfoRules, validateMovieInfo } = require('../middleware/movieInfoValidate.js');
+const {
+  movieInfoRules,
+  validateMovieInfo,
+} = require('../middleware/movieInfoValidate.js');
 const authenticateUser = require('../middleware/authorizeUser.js');
 
-router.get(
-    '/',
-    AsyncHandler(movieInfoController.getAllMovies));
+router.get('/', AsyncHandler(movieInfoController.getAllMovies));
 
-router.get(
-    '/:id',
-    AsyncHandler(movieInfoController.getMovieById));
+router.get('/:id', AsyncHandler(movieInfoController.getMovieById));
 
 router.put(
-    '/:id',
-    authenticateUser('admin'),
-    movieInfoRules(),
-    validateMovieInfo,
-    AsyncHandler(movieInfoController.updateMovieInfo));
+  '/:id',
+  authenticateUser('admin'),
+  movieInfoRules(),
+  validateMovieInfo,
+  AsyncHandler(movieInfoController.updateMovieInfo)
+);
 
 router.post(
-    '/',
-    authenticateUser('admin'),
-    movieInfoRules(),
-    validateMovieInfo,
-    AsyncHandler(movieInfoController.addMovie));
+  '/',
+  authenticateUser('admin'),
+  movieInfoRules(),
+  validateMovieInfo,
+  AsyncHandler(movieInfoController.addMovie)
+);
 
 router.delete(
-    '/:id',
-    authenticateUser('admin'),
-    AsyncHandler(movieInfoController.deleteMovie));
+  '/:id',
+  authenticateUser('admin'),
+  AsyncHandler(movieInfoController.deleteMovie)
+);
 
 module.exports = router;
