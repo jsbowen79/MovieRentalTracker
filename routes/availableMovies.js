51d79f21c82d4088cca572f77b47e1a6a197f28d
jsbowen79 <jsbowen79@outlook.com>
2026-06-13 15:13:53 -0600

    fix capitalization in route

--- routes/availableMovies.js
+++ routes/availableMovies.js
@@ -29,7 +29,7 @@ router.get(
 router.put(
   '/:id',
   authorizeUser('admin'),
-  UpdateAvailableMovieRules(),
+  updateAvailableMovieRules(),
   validateAvailableMovie,
   asyncHandler(availableMoviesController.updateAvailableMovie)
 );
