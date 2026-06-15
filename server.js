f79adead697a6dd32a637c0637505781e38f50fa
jsbowen79 <jsbowen79@outlook.com>
2026-06-15 11:24:42 -0600

    remove extra errorhandler call

--- server.js
+++ server.js
@@ -46,7 +46,6 @@ app.get('/', (req, res) => {
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 app.use('/', routes);
 
-app.use(errorHandler);
 //Start Server
 
 app.use(errorHandler);
